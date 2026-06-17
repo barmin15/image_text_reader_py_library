e55dc4957249a15a0dd15141b186838bef64a31b
Bokor Ármin <115711430+bokorarmin@users.noreply.github.com>
2026-06-17 16:40:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="image_text_reader",
     version="1.0.1",
     author="barmin15",
-    author_email="bokora@lauderalumni.hu",
+    author_email="dummy@email.com",
     description="A library to read text from images",
     long_description=long_description,
     long_description_content_type="text/markdown",
