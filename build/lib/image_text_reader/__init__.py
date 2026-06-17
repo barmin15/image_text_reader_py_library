1e1e6ceaf864b11b0b312bd1b2860c027ddc0522
Bokor Armin <bokora@lauderalumni.hu>
2026-06-17 16:56:52 +0200

    chore bump release

--- build/lib/image_text_reader/__init__.py
+++
@@ -1,1 +0,0 @@
-from .image_text_reader import ocr_image
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="image_text_reader",
-    version="1.0.1",
+    version="1.1.0",
     author="barmin15",
     author_email="armin@bokorarmin.com",
     description="A library to read text from images",
